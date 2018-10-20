e4d9809be7f678fddb88e761e21be70262c51cc9
Josh Spicer <hello@joshspicer.com>
2018-10-20 02:40:56 +0200

    added gps coords

--- App.js
+++ App.js
@@ -1,6 +1,58 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
 import MapView from 'react-native-maps';
+
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+    );
+  }
+
+
+
+  render() {
+   return(
+     <View style={styles.container}>
+     <MapView
+       style={styles.map}
+       region={{
+         latitude: this.state.latitude || 0,
+         longitude: this.state.longitude || 0,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+     <Text>Latitude: {this.state.latitude}</Text>
+     <Text>Longitude: {this.state.longitude}</Text>
+     {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+     <Text>
+
+     </Text>
+   </View>);
+ }
+}
 
 const styles = StyleSheet.create({
  container: {
@@ -15,17 +67,4 @@ const styles = StyleSheet.create({
  },
 });
 
-export default () => (
-   <View style={styles.container}>
-     <MapView
-       style={styles.map}
-       region={{
-         latitude: 37.78825,
-         longitude: -122.4324,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-     </MapView>
-   </View>
-);
+export default App;
