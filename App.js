63ea226ae775fa962f40557ee4929730b5ee9ea4
Josh Spicer <hello@joshspicer.com>
2018-10-20 11:07:53 +0200

    drawing arbitrary markers

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React, { Component }  from 'react';
+import _ from 'lodash';
 import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
-
+const fakeData = [
+  {name:"Play Football", date: "2018-10-21T01:01:29Z",latitude:41.3899301, longitude:2.1130454,description:"this is a test 1",creatorPhone:123456789},
+  {name:"Ukelele Jam Session", date: "2018-10-21T01:01:29Z",latitude:41.3889301, longitude:2.1130454,description:"this is a test 2",creatorPhone:54321}]
 
 class App extends Component {
 
@@ -17,6 +20,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    // Gets the current location and stores it in state.
     navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({
@@ -30,28 +34,38 @@ class App extends Component {
   }
 
 
-
   render() {
-   return(
-     <View style={styles.container}>
-     <MapView
-       style={styles.map}
-       region={{
-         latitude: this.state.latitude || 0,
-         longitude: this.state.longitude || 0,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-     >
-     </MapView>
-     <Text>Latitude: {this.state.latitude}</Text>
-     <Text>Longitude: {this.state.longitude}</Text>
-     {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
-     <Text>
+    const { latitude, longitude } = this.state;
+    const latLng =  {latitude: latitude, longitude: longitude};
 
-     </Text>
-   </View>);
- }
+    if (latitude != null || longitude != null) {
+        return(
+          <View style={styles.container}>
+            <MapView
+              style={styles.map}
+              region={{
+              latitude: latitude,
+              longitude: longitude,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}>
+
+            {fakeData.map((marker,key) => {
+              const coords = {latitude: marker.latitude, longitude: marker.longitude}
+               return(
+                 <MapView.Marker
+                      coordinate={coords}
+                      title={marker.name}
+               />
+             )})}
+
+          </MapView>
+        </View>);
+      } else {
+        console.log("loading");
+        return <Text>loading...</Text>
+      }
+    }
 }
 
 const styles = StyleSheet.create({
